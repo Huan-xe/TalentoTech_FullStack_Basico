2a9c6acb7596f4028bbef11573443d7a28cd3419
Juan Malaver <huanxe.tech@gmail.com>
2024-10-11 17:37:23 -0500

    Actualizando 11 oct 24

---
+++ Material Pedagógico/Modulo 3/Clase 1/Actividad-1/funciones.js
@@ -0,0 +1,9 @@
+function calcular(x,y){
+    console.log(`El producto de ${x} y ${y} es ${x * y}` )
+}
+calcular(5,5)
+
+let calcularValores = (x,y) => {
+    console.log(`El producto de ${x} y ${y} es ${x * y}`  )
+}
+calcularValores(10, 10)
