7774d7d10997b0990e17b5907a756d6d2f6457bc
Juan Malaver <huanxe.tech@gmail.com>
2024-10-11 18:10:50 -0500

    Actualizando 11 oct 24

--- Material Pedagógico/Modulo 3/Clase 1/Actividad-1/funciones.js
+++ Material Pedagógico/Modulo 3/Clase 1/Actividad-1/funciones.js
@@ -3,7 +3,22 @@ function calcular(x,y){
 }
 calcular(5,5)
 
+// Funcion anonima.
 let calcularValores = (x,y) => {
     console.log(`El producto de ${x} y ${y} es ${x * y}`  )
 }
 calcularValores(10, 10)
+
+let fun  = function (a, b){
+    return a + b;
+}
+let total = fun(2,3);
+console.log(total);
+
+(function (a, b){
+    console.log(a + b);
+    console.log(a - b);
+    console.log(a * b);
+    console.log(a / b);
+    
+}) (3,4);
