c45d5991e484bce606f8089ac8625d44602c3943
Juan Malaver <huanxe.tech@gmail.com>
2024-10-15 18:26:20 -0500

    Actualizacion modulo 3 clase 5

---
+++ Material Pedagógico/Modulo 3/Clase 3/Actividad-3/actividad3.js
@@ -0,0 +1,21 @@
+let celsius;
+let fahrenheit;
+
+function celsiusToFahrenheit(celsius){
+
+
+    celsius = document.getElementById(elementId: "celsius").value
+
+
+    fahrenheit=celsius*(9/5)+32;
+
+    document.getElementById(elementId:"fahrenheit").value = f;
+}
+
+function fahrenheitToCelsius(fahrenheit){
+
+
+    fahrenheit =  document.getElementById(elementId:"fahrenheit").value = f;
+
+    celsius=(fahrenheit-32)();
+}
